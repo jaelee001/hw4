255d04c756c6e81a513f5d349d17e2144a998a02
jaelee001 <jaehee.lee@kellogg.northwestern.edu>
2022-03-16 03:39:52 +0000

    attempt_1

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.where({ email:params["email"]})[0]
+    if @user
+      if BCrypt::Password.new(@user.password) == params["password"]
+        flash[:notice] = "You logged in!"
+        redirect_to "/"
+      else
+        flash[:notice] = "Please try again"
+        redirect_to "/sessions/new"
+      end
+    else
+      flash[:notice] = "Please try again"
+      redirect_to "/sessions/new"
+    end
   end
 
   def destroy
